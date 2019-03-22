fbc777aa10970c2ab169b1d6e8331bc52a900202
Alec Chen <goodneighboralec@gmail.com>
2019-03-22 00:44:57 -0700

    testing mongoDB connection

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const {ApolloServer} = require ('apollo-server')
-
-
 const typeDefs = require('./typeDefs')
-
 const resolvers = require('./resolvers')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI,  {userNewUrlParser: true })
+    .then(() => console.log("DB connected"))
+    .catch(err => console.log(err));
 
 const server = new ApolloServer({
     
     typeDefs,
     resolvers
-} )
+});
 
 
-server.listen()
+server.listen().then (({url}) => {
+    console.log(`server listening on ${url}`);
+});
